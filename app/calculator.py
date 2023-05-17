7682376c5e625c78e7251d79da34031b0aa61e84
Juan Ángel Dausa <juandausa@gmail.com>
2023-05-17 15:10:41 -0300

    Fisrt commit. Base project with tests and coverage.

---
+++ app/calculator.py
@@ -0,0 +1,29 @@
+class Calculator:
+    # init method or constructor
+    def __init__(self):
+        return
+
+    def add(self, x, y):
+        return x + y
+
+    def subtract(self, x, y):
+        return x - y
+
+    def multiply(self, x, y):
+        return x * y
+
+    def pow(self, x, y):
+        result = 1
+
+        if y == 0:
+            return result
+        
+        for i in range(y):
+            result = self.multiply(result, x)
+
+        return result
+
+    def divide(self, x, y):
+        if y == 0:
+            return 'Cannot divide by 0'
+        return x * 1.0 / y
