526ce0bdd3415987ecdbe81f77bce181597e709c
Juan Ángel Dausa <juandausa@gmail.com>
2023-05-17 15:43:15 -0300

    Add the responses package to mock requests in the test env.

--- app/test_pokemon_api.py
+++ app/test_pokemon_api.py
@@ -1,33 +1,46 @@
 import unittest
+import responses
 from unittest.mock import patch
 from .pokemon_api import PokemonAPI
+
+
+'''
+import responses
+import requests
+
+@responses.activate
+def test_simple():
+    responses.add(responses.GET, 'http://twitter.com/api/1/foobar',
+                  json={'error': 'not found'}, status=404)
+
+    resp = requests.get('http://twitter.com/api/1/foobar')
+
+    assert resp.json() == {"error": "not found"}
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == 'http://twitter.com/api/1/foobar'
+    assert responses.calls[0].response.text == '{"error": "not found"}'
+'''
 
 class PokemonAPITest(unittest.TestCase):
     def setUp(self):
         self.api = PokemonAPI()
 
-    @patch('pokemon_api.requests.get')
-    def test_get_pokemon_info_valid(self, mock_get):
-        mock_response = mock_get.return_value
-        mock_response.status_code = 200
-        mock_response.json.return_value = {
-            'id': 25,
-            'types': [
-                {'type': {'name': 'electric'}},
-            ]
-        }
-
+    def test_get_pokemon_info_valid(self):
+        responses.add(responses.GET, 'https://pokeapi.co/api/v2/pokemon/25', 
+                    json={
+                        'id': 25,
+                        'types': [
+                            {'type': {'name': 'electric'}},
+                        ]
+                    },
+                    status=200)
+        
         pokemon_info = self.api.get_pokemon_info(25)
 
         self.assertEqual(pokemon_info, (25, ['electric']))
-        mock_get.assert_called_once()
 
-    @patch('pokemon_api.requests.get')
-    def test_get_pokemon_info_invalid(self, mock_get):
-        mock_response = mock_get.return_value
-        mock_response.status_code = 404
-
-        pokemon_info = self.api.get_pokemon_info(1000)
+    def test_get_pokemon_info_invalid(self):
+        pokemon_info = self.api.get_pokemon_info(2000)
 
         self.assertIsNone(pokemon_info)
-        mock_get.assert_called_once()
